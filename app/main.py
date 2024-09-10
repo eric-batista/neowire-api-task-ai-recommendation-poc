4ee3b095481c177ec9ff7745e0438583d984b906
Eric Batista <dev.eric.batista@gmail.com>
2024-09-10 21:59:11 +0000

    fix(cors): add cors middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.schemas.recommendation_schema import RecommendationInput, RecommendationOutput
 from app.services.recommendation_service import RecommendationService
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens. Modifique conforme necessário.
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP, incluindo OPTIONS e POST.
+    allow_headers=["*"],  # Permite todos os cabeçalhos.
+)
 # Instancia o serviço de recomendação
 recommendation_service = RecommendationService()
 
@@ -17,3 +25,8 @@ def get_recommendations(input: RecommendationInput):
     
     # Retorna as recomendações em formato JSON
     return {"recommended_tasks": recommendations}
+
+@app.options("/recommendations/")
+def get_recommendations():
+    return {"ok": True}
+    
